7dccc1774a9b564bc6d57716cc2eef2ab2a7b7e7
Ayan Sahil <ayans8557@gmail.com>
2025-11-09 19:34:24 +0530

    DSA day-2

--- script.js
+++ script.js
@@ -1,20 +1,58 @@
-// 1. sum of two integer
+//1.sum of two integer
 // let a=10;
 // let b=20;
-// console.log("sum of 10 and 20 is " +(a+b));
+// let sum=a+b;
+// console.log(sum);
 
-// 2.Relation between integer and string
+//2.Relation between integer and string
 
-//Sum and message
-
-// type coercion in js:-
 // console.log("1" + 1); //11
-
 // 100 + true;  // 100 + 1, Result: 101
 // 5 + false;   // 5 + 0, Result: 5
-
 // console.log("1" - 1); //0
 // console.log("1" * 1); //1
 // console.log("1" / 1); //1
 // console.log("1" % 1); //0
-// console.log("1" **1); //1   
+// console.log("1" **1); //1
+
+//3. sum and message.
+// (type coercion in js:-)
+// let a=10;
+// let b=20;
+// console.log("sum of 10 and 20 is " +(a+b));
+
+//4. Accept and print the answer
+
+// let age = Number(prompt("enter your age"));
+// console.log(age);
+
+//5.Swap two variables by 3 methods :
+
+//method 1
+
+// let a=10;
+// let b=20;
+// let c;
+// c=a;// c=10, a=10
+// a=b;//a=20 ,b=20
+// b=c;//b=10
+// console.log(a,b);
+
+
+// method2
+
+// let a=10;
+// let b=20;
+// [a,b]=[b,a];
+// console.log(a,b);
+
+
+// method3
+
+// let a=10;
+// let b=20;
+
+// a=a+b; //10+20=30
+// b=a-b; //30-20=10
+// a=a-b; //30-10=20
+// console.log(a,b);
